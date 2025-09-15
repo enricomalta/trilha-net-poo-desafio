269ac9da183bd4164f3411514176c201c6d3f4ee
Enrico <enrico.malta1@gmail.com>
2025-09-15 11:23:13 -0300

    Atualizei as classes de Smartphone e adicionei melhorias no Program.cs

--- Program.cs
+++ Program.cs
@@ -1,3 +1,16 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+class Program
+{
+    static void Main(string[] args)
+    {
+        Smartphone nokia = new Nokia(numero: "11999999999", modelo: "Nokia 3310", imei: "123456789", memoria: 32);
+        Smartphone iphone = new Iphone(numero: "11988888888", modelo: "iPhone 13", imei: "987654321", memoria: 128);
+
+        nokia.Ligar();
+        nokia.InstalarAplicativo("WhatsApp");
+
+        iphone.ReceberLigacao();
+        iphone.InstalarAplicativo("Instagram");
+    }
+}
